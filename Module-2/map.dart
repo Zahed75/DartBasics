7826c8bb90fb1c8c8e2c579f3df321486db21b19
Zahed75 <zahedhasan.cs@gmail.com>
2023-05-09 19:15:49 +0600

    map added

---
+++ Module-2/map.dart
@@ -0,0 +1,12 @@
+void main() {
+  var devInfo = {
+    "name": "Zahed Hasan",
+    "dept": "Engineering",
+    "company": "Syscomatic Technologies",
+    "salary": "Null"
+  };
+
+  devInfo["location"] = "Dhanmondi";
+
+  print(devInfo);
+}
