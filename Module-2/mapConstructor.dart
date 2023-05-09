cd5d3adb482f96f9ba68e26ef110d81a9a487b6c
Zahed75 <zahedhasan.cs@gmail.com>
2023-05-09 19:26:38 +0600

    map constructor  added

---
+++ Module-2/mapConstructor.dart
@@ -0,0 +1,16 @@
+void main() {
+  var devInfo = {
+    "name": "Zahed Hasan",
+    "dept": "Engineering",
+    "company": "Syscomatic Technologies",
+    "salary": "Null"
+  };
+
+  devInfo["location"] = "Dhanmondi";
+
+  print(devInfo.keys);
+  print(devInfo.values);
+  print(devInfo.length);
+  print(devInfo.isEmpty);
+  print(devInfo.isNotEmpty);
+}
