d8304b0248ab1e37ffc81bd756f7beb02b466c15
Zahed75 <zahedhasan.cs@gmail.com>
2023-05-13 05:18:00 +0600

    switch case done

---
+++ Module-2/switch.dart
@@ -0,0 +1,24 @@
+void main() {
+  var marks = 39;
+
+  switch (marks) {
+    case 80:
+      print("A+");
+      break;
+
+    case 70:
+      print("A");
+      break;
+
+    case 60:
+      print("A-");
+      break;
+    case 50:
+      print("B");
+      break;
+
+    default:
+      print("Result Not Found");
+      break;
+  }
+}
